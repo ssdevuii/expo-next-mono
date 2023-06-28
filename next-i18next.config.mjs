b767e6e335abc59a9b80a7c9f6f5f412155510fe
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2023-06-28 12:46:59 +0700

    feat: :construction: i18n w

--- next-i18next.config.mjs
+++ next-i18next.config.mjs
@@ -3,4 +3,5 @@ const isBrowser = typeof window !== "undefined";
 /** @type {import('next-i18next').UserConfig} */
 export const i18n = {
   i18n: { locales: ["en", "id"], defaultLocale: "id" },
+  backend: {}
 };
