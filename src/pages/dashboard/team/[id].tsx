10075a16dc340f35172cd3d45cab2bbf7c2b2f93
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2023-07-08 02:12:22 +0700

    feat: :bug: add warning

--- src/pages/dashboard/team/[id].tsx
+++ src/pages/dashboard/team/[id].tsx
@@ -128,6 +128,27 @@ const TeamEditForm = () => {
               />
             )}
 
+            <div className="mb-8 flex items-center gap-2 text-red-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+                />
+              </svg>
+              <p>
+                Input ini tidak usah diisi jika tidak ingin mengubah data Dosen atau
+                Matakuliah!
+              </p>
+            </div>
+
             {subjectAndLecturer.map(({ lecturerId, subjectId }, i) => (
               <MultipleInputWrapper key={i} className="double_select_container">
                 <Select
