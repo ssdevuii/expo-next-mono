3fa89d4dbf7581221adf1fa51d993758b0fbf3dd
maziazi <mazis9651@gmail.com>
2024-05-29 21:55:10 +0700

    coba-coba

---
+++ prisma/migrations/20240529144800_hapus/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `afsar` on the `Example` table. All the data in the column will be lost.
+  - You are about to drop the column `siraj` on the `Example` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Example` DROP COLUMN `afsar`,
+    DROP COLUMN `siraj`;
